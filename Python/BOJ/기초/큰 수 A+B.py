c7db77439bd19b512197577a109224828f322fad
kmonguu <50946547+kmonguu@users.noreply.github.com>
2021-08-27 15:18:40 +0900

    Create 큰 수 A+B.py
    큰 수 A+B 코드 풀이

---
+++ Python/BOJ/기초/큰 수 A+B.py
@@ -0,0 +1,11 @@
+# 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 문제
+# 예를 들어, A = 9223372036854775807, B = 9223372036854775808 일 때 결과를 출력할 수 있어야한다.
+
+import sys                                       # sys 모듈 사용
+
+A, B = map(int, sys.stdin.readline().split())
+print(A+B)
+
+
+# 다른 풀이)
+print(sum(map(int, input().split())))            # sys 모듈을 사용하지 않고 내장 함수 sum을 사용하여 풀 수 있다.

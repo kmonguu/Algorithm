ee65cef88f0e106fc84b2653b1c4ff83258019a5
kmonguu <50946547+kmonguu@users.noreply.github.com>
2021-09-15 11:30:18 +0900

    Update 1. 수 정렬하기.py

--- Python/BOJ/정렬/1. 수 정렬하기.py
+++ Python/BOJ/정렬/1. 수 정렬하기.py
@@ -12,9 +12,28 @@ for _ in range(N):
   s = sorted(s)                   # s변수에 있는 값들을 정렬하여 다시 s에 저장
   
 for i in s:                       # s배열에 있는 요소를 for문을 이용하여 i에 선언
-    print(i)          
+    print(i)     
     
     
+    
+    
+    
+# 다른 풀이
+
+N = int(input())
+s = sorted([int(input()) for _ in range(N)])  # 리스트 컴프리헨션과 sorted 함수를 이용하여 입력 받은 수 오름차순 정렬
+print("\n".join(map(str, s)))                 # s의 모든 요소를 문자로 변환 후 개행문자를 기준으로 1\n2\n3\n4\n5 이렇게 join
+    
+  
+  
+  
+# 위의 코드를 한 줄로도 나타낼 수 있다.
+
+print("\n".join(map(str, sorted([int(input()) for _ in range(int(input()))]))))
+
+
+
+
 # 입력
 5     # 입력받을 수의 갯수
 5

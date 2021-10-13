bda0f556f1d8471589e52d2c9ce29232478759db
kmonguu <50946547+kmonguu@users.noreply.github.com>
2021-10-13 22:41:12 +0900

    Update 큐.py

--- Python/BOJ/큐, 덱/큐.py
+++ Python/BOJ/큐, 덱/큐.py
@@ -52,3 +52,62 @@ for _ in range(int(input())):
       print(queue[-1])
     else:
       print(-1)
+      
+      
+      
+      
+# 다른 풀이 : if - else를 삼항연산자로 수정(가독성 증가)
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+queue = deque()
+for _ in range(int(input())):
+  order = input().split()
+
+  if order[0] == 'push':
+    queue.append(order[1])
+
+  elif order[0] == 'pop':
+    print(queue.popleft() if queue else -1)             # 만약 queue가 비어있지 않다면 pop, 아닐 경우 -1출력
+
+  elif order[0] == 'size':
+    print(len(queue))
+
+  elif order[0] == 'empty':
+    print(0 if queue else 1)                            # queue가 비어있지 않다면 0을 출력, 아닐 경우 -1출력 
+
+  elif order[0] == 'front':
+    print(queue[0] if queue else -1)                    # queue가 비어있지 않다면 queue배열의 0번째 인덱스 요소 출력, 아닐 경우 -1 출력
+
+  elif order[0] == 'back':
+    print(queue[-1] if queue else -1)                   # queue가 비어있지 않다면 queue배열의 가장 뒤의 있는 요소 출력, 아닐 경우 -1 출력
+    
+    
+    
+    
+    
+    
+# 다른 풀이 : lambda 식을 이용한 방법
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+queue = deque()
+command = {                                                       # 명령어 'push', 'pop', 'size', 'empty', 'front', 'back'을 딕셔너리의 key값으로 설정
+  "push": lambda x: queue.append(x),                              # lmabda 식을 이용하여 해당 명령을 처리하여 나온 값을 value로 설정 
+  "pop": lambda: print(queue.popleft() if queue else -1),
+  "size": lambda: print(len(queue)),
+  "empty": lambda: print(0 if queue else 1),
+  "front": lambda: print(queue[0] if queue else -1),
+  "back": lambda: print(queue[-1] if queue else -1)
+}
+
+for _ in range(int(input())):
+  order = input().split()
+  if len(order) == 2:                                             # 입력받은 order의 길이가 2인 경우(예를 들어, push 2)
+    command[order[0]](order[1])                                   # order[0]은 'push'를 의미하므로 command에서 key가 'push'일 때 order[1] 값을 
+  else:
+    command[order[0]]()

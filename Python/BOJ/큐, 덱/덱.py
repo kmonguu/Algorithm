fffb9004e25e356654035aebbf8dd1f835892272
kmonguu <50946547+kmonguu@users.noreply.github.com>
2021-11-03 17:39:44 +0900

    Update 덱.py

--- Python/BOJ/큐, 덱/덱.py
+++ Python/BOJ/큐, 덱/덱.py
@@ -11,3 +11,49 @@
 
 # 코드
 
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+queue = deque()
+for _ in range(int(input())):
+  order = input().split()
+
+  if order[0] == 'push_front':
+    queue.appendleft(order[1])
+
+  elif order[0] == 'push_back':
+    queue.append(order[1])
+
+  elif order[0] == 'pop_front':
+    if queue:
+      print(queue.popleft())
+    else:
+      print(-1)
+
+  elif order[0] == 'pop_back':
+    if queue:
+      print(queue.pop())
+    else:
+      print(-1)
+
+  elif order[0] == 'size':
+    print(len(queue))
+
+  elif order[0] == 'empty':
+    if queue:
+      print(0)
+    else:
+      print(1)
+
+  elif order[0] == 'front':
+    if queue:
+      print(queue[0])
+    else:
+      print(-1)
+
+  elif order[0] == 'back':
+    if queue:
+      print(queue[-1])
+    else:
+      print(-1)

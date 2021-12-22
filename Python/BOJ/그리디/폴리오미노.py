41f645203221c57d69e3ec78fa2a8edabf51415f
kmonguu <50946547+kmonguu@users.noreply.github.com>
2021-12-22 16:47:48 +0900

    Update 폴리오미노.py

--- Python/BOJ/그리디/폴리오미노.py
+++ Python/BOJ/그리디/폴리오미노.py
@@ -30,3 +30,28 @@ else:
 # 다른 풀이
 board = input().replace("XXXX", "AAAA").replace("XX", "BB")
 print(-1 if "X" in board else board)
+
+
+
+# 다른 풀이
+# A로 먼저 덮고, B를 덮는 함수
+# 예를 들어 XXXXXX 이면 l은 6이고, "A" * 4 * (6 // 4) + "B" * (l % 4)으로 A 먼저 덮고 B를 덮을 수 있음
+
+board = input()
+
+def convert(l):
+  return ("A" * 4 * (l // 4) + "B" * (l % 4)) if not l % 2 else -1          # l 만큼 모두 덮을 수 있으면 덮은 결과, 덮을 수 없으면 -1 return
+
+result = ""
+l = 0
+for b in board:
+  if b == "X":
+    l += 1                                                                  # X의 길이 구함
+  else:
+    tmp = convert(l)                                                        # 덮은 문자열로 변환
+    result += ("" if tmp == -1 else (tmp + b))                              # l 만큼 덮지 못하였으면 "", 덮었으면 tmp + "."을 result에 더함
+    l = 0                                                                   # l 초기화
+
+tmp = convert(l)                                                            # 남은 문자열에 대한 길이에 대해 덮은 문자열로 변환
+result += ("" if tmp == -1 else tmp)                                        # l 만큼 덮지 못하였으면 "", 덮었으면 tmp을 result에 더함
+print(result if len(result) == len(board) else -1)                          # result와 board의 길이가 같으면 result, 다르면 -1 출력

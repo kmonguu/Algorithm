df23c8dfa84cfcaab1ee5fecb9926dc9d78f7c6d
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-01-04 09:17:59 +0900

    Update 덩치.py

--- Python/BOJ/구현/덩치.py
+++ Python/BOJ/구현/덩치.py
@@ -31,3 +31,23 @@ for i in people:
       rank += 1                                     # rank + 1
   print(rank, end=" ")
 
+
+  
+  
+# 다른 풀이
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+info = [list(map(int, input().split())) for _ in range(n)]
+
+result = []
+for i in range(n):
+  rank = 1
+  for j in range(n):
+    if info[i][0] < info[j][0] and info[i][1] < info[j][1]:
+      rank += 1
+  
+  result.append(rank)
+
+print(*result)

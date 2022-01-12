3df7896d69e787a7c77bc746c30bb2338209f5d2
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-01-12 14:12:20 +0900

    Update 미로 탐색.py

--- Python/BOJ/너비우선탐색/미로 탐색.py
+++ Python/BOJ/너비우선탐색/미로 탐색.py
@@ -38,3 +38,37 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 print(bfs(0, 0))
+
+
+
+
+# 정리된 코드
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+  queue = deque()
+  queue.append((x, y))
+
+  while queue:
+    x, y = queue.popleft()
+
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+
+      if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1:
+        board[nx][ny] = board[x][y] + 1
+        queue.append((nx, ny))
+
+  return board[n-1][m-1]
+
+n, m = map(int, input().split())
+board = [list(map(int, input().strip())) for _ in range(n)]
+
+print(bfs(0, 0))

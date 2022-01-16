4b9775fb61461879c3ed6cb3ee28e156edaa3ad8
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-01-16 22:06:52 +0900

    Update 집합.py

--- Python/BOJ/구현/집합.py
+++ Python/BOJ/구현/집합.py
@@ -35,3 +35,28 @@ for _ in range(m):
 
   elif order[0] == "empty":
     s = []
+    
+    
+    
+# 다른 풀이
+# command 딕셔너리를 만들고, key에 명령어 이름, value에 lambda 식 넣어 푸는 방법
+
+import sys
+input = sys.stdin.readline
+
+s = set()
+all_set = set(map(str, range(1, 21)))
+command = {
+  "add": lambda x: s.add(x),
+  "remove": lambda x: s.discard(x),
+  "check": lambda x: print(1 if x in s else 0),
+  "toggle": lambda x: s.discard(x) if x in s else s.add(x),
+  "all": lambda: all_set,
+  "empty": lambda: set()
+}
+
+m = int(input())
+for _ in range(m):
+  c = input().strip().split()
+  if len(c) == 1: s = command[c[0]]()                             # c의 길이가 1일때는 명령어에 해당하는 함수 호출하고 s 갱신
+  else: command[c[0]](c[1])                                       # c의 길이가 2일때는 명령어에 해당하는 함수 호출

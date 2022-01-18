432a38bf3ec7867ab867f6b97a79c9568700b31e
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-01-18 16:46:29 +0900

    Create 핸드폰 번호 가리기.js

---
+++ JavaScript/Programmers/level 1/핸드폰 번호 가리기.js
@@ -0,0 +1,8 @@
+// 전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 * 으로 가린 문자열을 반환
+
+// 내가 푼 코드
+// phone_number의 전체 길이에서 4개를 뺀 나머지 요소를 '*'로 만들고 phone_number의 마지막 요소 4개를 더하여 반환한다.
+
+function solution(phone_number) {
+    return '*'.repeat(phone_number.length-4) + phone_number.slice(-4);
+}

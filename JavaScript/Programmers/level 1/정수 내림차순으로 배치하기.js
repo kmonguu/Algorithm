903206ec50b23351d40ab7f0154174723893c619
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-01-28 00:22:47 +0900

    Update 정수 내림차순으로 배치하기.js

--- JavaScript/Programmers/level 1/정수 내림차순으로 배치하기.js
+++ JavaScript/Programmers/level 1/정수 내림차순으로 배치하기.js
@@ -8,3 +8,15 @@ function solution(n)  {
     let answer = n.toString().split('').sort().reverse().map(x => parseInt(x))
     return Number(answer.join(''));
 }
+
+
+
+// 다른 사람 코드
+// toString 메소드를 이용하여 n을 문자열로 변환하고, 스프레드 문법을 이용하여 변환된 문자열을 배열로 변환한다.
+// 화살표 함수를 사용하여 문자열 배열을 내림차순으로 정렬한다.
+// join메소드를 사용하여 정렬된 배열을 하나의 문자열로 join해준다.
+// +로 정수로 변환하여 반환한다.
+
+function solution(n) {
+    return +[...n.toString()].sort((a, b) => b - a).join("");
+}

3177811f24ac809cc70134cab3dff2c96ae5db57
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-02-01 23:43:30 +0900

    Update 완전제곱수.py

--- Python/BOJ/구현/완전제곱수.py
+++ Python/BOJ/구현/완전제곱수.py
@@ -11,3 +11,36 @@ for i in range(m, n+1):
     answer.append(i)
 
 print(f'{sum(answer)}\n{min(answer)}' if answer else -1)
+
+
+
+# 다른 사람 풀이
+# m과 n 사이의 수를 모두 확인하지 않고 m과 n의 제곱근 값들만 확인
+
+import math
+m, n = math.ceil(int(input()) ** 0.5), int(int(input()) ** 0.5) + 1
+
+result = range(m, n)
+if result:
+  print(sum(map(lambda x: x ** 2, result)))
+  print(result[0] ** 2)
+else:
+  print(-1)
+  
+
+  
+  
+  
+# fuctools 모듈을 사용한 방법
+
+import math
+from functools import reduce
+
+m, n = math.ceil(int(input()) ** 0.5), int(int(input()) ** 0.5) + 1
+
+result = range(m, n)
+if result:
+  print(reduce(lambda total, cur: total + cur ** 2, result, 0))
+  print(result[0] ** 2)
+else:
+  print(-1)

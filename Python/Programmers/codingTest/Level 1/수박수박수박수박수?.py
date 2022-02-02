4d9f5192f27673db5ee3a23eda37fc423a1af112
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-02-02 22:48:57 +0900

    Create 수박수박수박수박수?.py

---
+++ Python/Programmers/codingTest/Level 1/수박수박수박수박수?.py
@@ -0,0 +1,17 @@
+# 길이가 n이고 '수박수박수박수..'와 같은 패턴을 유지하는 문자열을 반환
+
+# 내가 푼 코드
+# '수'와 '박'을 배열 안에 넣고, 0부터 매개변수로 주어지는 정수 n까지 순회하며,
+# 짝수라면 '수'를 answer에 저장, 홀수라면 '박'을 answer에 저장한다.
+
+def solution(n):
+    answer = ''
+    
+    arr = ['수', '박']
+    for i in range(n):
+        if i % 2 == 0:
+            answer += arr[0]
+        else:
+            answer += arr[1]
+    return answer
+        

1ee7695de815b4482d8fb6a37107d5827c50a027
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-02-03 15:02:22 +0900

    Update 이항계수.py

--- Python/BOJ/구현/이항계수.py
+++ Python/BOJ/구현/이항계수.py
@@ -10,3 +10,19 @@ def fect(x):
   return x * fect(x-1) if x > 1 else 1
 
 print(int(fect(n)/(fect(n-k)*fect(k))))
+
+
+
+# 다른 사람 풀이
+# 재귀를 사용하지 않고, 다이나믹 알고리즘을 사용
+
+def c(n, k):
+  dp = [0] * (n + 1)
+  dp[0] = dp[1] = 1
+  for i in range(2, n + 1):
+    dp[i] = i * dp[i - 1]
+
+  return dp[n] // (dp[k] * dp[n - k])
+
+n, k = map(int, input().split())
+print(c(n, k))

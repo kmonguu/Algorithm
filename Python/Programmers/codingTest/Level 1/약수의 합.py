e70dc57e4367619109f55f03e0a64a5e77a3e772
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-02-04 17:21:58 +0900

    Update 약수의 합.py

--- Python/Programmers/codingTest/Level 1/약수의 합.py
+++ Python/Programmers/codingTest/Level 1/약수의 합.py
@@ -9,3 +9,10 @@ def solution(n):
         if n % num == 0:
             answer += num
     return answer
+
+
+
+# 다른 사람 풀이
+
+def solution(n):
+    return sum([d + (0 if d == (n // d) else (n // d)) for d in range(1, int(n ** 0.5) + 1) if n % d == 0])

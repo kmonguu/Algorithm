7f5215a9a195941b18949da38c5fa469928ba0e2
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-03-14 23:37:27 +0900

    Create 나머지가 1이 되는 수 찾기.js

---
+++ JavaScript/Programmers/level 1/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,18 @@
+// 자연수 n이 매개변수로 주어질 때, n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 수 x를 반환
+
+// 내가 푼 코드
+
+function solution(n) {
+    for (let i = 2; i <= n; i++){
+        if (n % i == 1) {
+            return i;
+        }
+    }
+
+
+// 다른 사람 코드
+function solution(n) {
+    for (let i = parseInt(n ** 0.5); i <= n; i++){
+        if (n % i == 1) return i;
+    }
+}

28fec679eacdc4803d228629efa5d1dd00cd3b27
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-03-15 23:33:42 +0900

    Update 나머지가 1이 되는 수 찾기.js

--- JavaScript/Programmers/level 1/나머지가 1이 되는 수 찾기.js
+++ JavaScript/Programmers/level 1/나머지가 1이 되는 수 찾기.js
@@ -9,8 +9,9 @@ function solution(n) {
         }
     }
 
-
 // 다른 사람 코드
+// 나머지가 1이 되도록 하는 가장 작은 수 x를 찾는 범위를 n의 제곱근으로 설정
+    
 function solution(n) {
     for (let i = parseInt(n ** 0.5); i <= n; i++){
         if (n % i == 1) return i;

9bef7efc6200658a1270cfb9855fa498ee4602b7
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-03-28 22:06:55 +0900

    Update 피보나치 수.py

--- Python/Programmers/codingTest/Level 2/피보나치 수.py
+++ Python/Programmers/codingTest/Level 2/피보나치 수.py
@@ -11,3 +11,15 @@ def solution(n):
         fibo.append((fibo[i-2] + fibo[i-1]) % 1234567)
     
     return fibo[-1]
+
+
+# 다른 사람 풀이
+# 다이나믹 프로그래밍(dp) 알고리즘의 정석대로 푼 방법
+
+def solution(n):
+    dp = [0] * (n + 1)
+    dp[0], dp[1] = 0, 1
+    for i in range(2, n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
+    
+    return dp[n]

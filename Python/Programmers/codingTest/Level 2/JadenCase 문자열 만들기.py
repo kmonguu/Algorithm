b814ef43f56a3543ad178809684c8108c266c134
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-05-05 22:38:27 +0900

    Update JadenCase 문자열 만들기.py

--- Python/Programmers/codingTest/Level 2/JadenCase 문자열 만들기.py
+++ Python/Programmers/codingTest/Level 2/JadenCase 문자열 만들기.py
@@ -5,7 +5,7 @@
 # 문자열 s를 공백을 기준으로 잘라 배열을 만들고 단어들로 구성된 새로운 배열을 생성
 # 새로 만들어진 배열을 순회하며 각 단어의 첫글자를 capitalize()를 사용하여 JadenCase로 만들고 answer에 저장 후 반환
 
-# <capitalize()와 title의 차이>
+# <capitalize와 title의 차이>
 # capitalize() : 문장의 첫 단어의 첫 글자만 대문자로 바꿈
 # title() : 문장의 모든 단어의 첫 글자를 대문자로 바꿈
 

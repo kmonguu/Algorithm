379b9aa0e64e7683b37d1481d3b18d2e863afa22
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-05-30 23:52:17 +0900

    Update 소수 찾기.py

--- Python/Programmers/codingTest/Level 2/소수 찾기.py
+++ Python/Programmers/codingTest/Level 2/소수 찾기.py
@@ -4,3 +4,24 @@
 # numbers = "17" 이라고 할 때, [1, 7]로는 소수 [7, 17, 71]을 만들 수 있기 때문에 3이 반환된다.
 
 # 내가 푼 코드
+from itertools import permutations
+
+def solution(numbers):
+    number = []
+    for i in range(1, len(numbers) + 1):
+        number += list(permutations(numbers, i))
+        
+    answer = set([int(''.join(num)) for num in number])
+    
+    result = 0
+    for a in answer:
+        cnt = 0
+        for i in range(2, int(a**0.5)+1):
+            if a % i == 0:
+                cnt += 1
+                
+        if a != 0 and a != 1 and cnt == 0:
+            result += 1
+                
+                
+    return result

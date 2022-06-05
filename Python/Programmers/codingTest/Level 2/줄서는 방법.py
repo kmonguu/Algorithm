6fe5d16b779cb7ccea2f7dd3bc526c7a51ade07f
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-06-05 20:03:41 +0900

    Update 줄서는 방법.py

--- Python/Programmers/codingTest/Level 2/줄서는 방법.py
+++ Python/Programmers/codingTest/Level 2/줄서는 방법.py
@@ -1,1 +1,16 @@
 
+from itertools import permutations
+
+def solution(n, k):
+    arr = []
+    answer = [num for num in range(1, n+1)]
+    
+    
+    for i in permutations(answer, len(answer)): 
+        arr.append(list(i))
+        
+        if len(arr) >= k:
+            break
+        
+    return arr[-1]
+        

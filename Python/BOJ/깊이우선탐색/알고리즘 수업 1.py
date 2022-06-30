ac811b7b379e12c13441dceb446ca39ab1816b9d
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-06-30 18:34:11 +0900

    Update 알고리즘 수업 1.py

--- Python/BOJ/깊이우선탐색/알고리즘 수업 1.py
+++ Python/BOJ/깊이우선탐색/알고리즘 수업 1.py
@@ -13,3 +13,33 @@
 # 3
 # 4
 # 0
+
+
+# 메모리 초과 코드
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+
+edge_info = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+count = 1
+
+for _ in range(m):
+  u, v = map(int, input().split())
+  edge_info[u].append(v)
+  edge_info[v].append(u)
+
+def dfs(n):
+  global count 
+  visited[n] = count
+  edge_info[n].sort()
+  for i in edge_info[n]:
+    if not visited[i]:
+      count += 1
+      dfs(i)
+      
+dfs(r)
+for cnt in range(1, n+1):
+  print(visited[cnt])

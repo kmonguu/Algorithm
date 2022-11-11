5cb2eb72ef655f9caa83344f20797cf1be23a72a
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-11-11 17:23:44 +0900

    Update 잘라서 배열로 저장하기.py

--- Python/Programmers/codingTest/Level 0/잘라서 배열로 저장하기.py
+++ Python/Programmers/codingTest/Level 0/잘라서 배열로 저장하기.py
@@ -1,7 +1,7 @@
 # 문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 반환
 
 # 예제
-# my_str	            n	  result
+# my_str                n     result
 # "abc1Addfggg4556b"	6	  ["abc1Ad", "dfggg4", "556b"]
 # "abcdef123"	        3	  ["abc", "def", "123"]
 

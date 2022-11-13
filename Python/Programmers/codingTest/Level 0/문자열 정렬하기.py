2ef6a0b2865bb192880c4d8644abab069192b3b4
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-11-13 13:23:14 +0900

    Create 문자열 정렬하기.py

---
+++ Python/Programmers/codingTest/Level 0/문자열 정렬하기.py
@@ -0,0 +1,14 @@
+# 영어 대소문자로 이루어진 문자열 my_string이 매개변수로 주어질 때, my_string을 모두 소문자로 바꾸고 알파벳 순서대로 정렬한 문자열을 반환
+
+# 예제
+"Bcad"   -> "abcd"
+"heLLo"  -> "ehllo"
+"Python" -> "hnopty"
+
+# 내가 푼 코드
+# 1. 문자열 속 대문자를 소문자로 변환
+# 2. 소문자 문자열을 알파벳 순서로 정렬
+# 3. 배열로 저장된 정렬된 알파벳을 문자열로 출력
+
+def solution(my_string):
+    return''.join(sorted(my_string.lower()))

b9227fedd18b2f71cb8b87e22d94c4bf7dcd9eac
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-11-21 18:40:49 +0900

    Update n의 배수 고르기.py

--- Python/Programmers/codingTest/Level 0/n의 배수 고르기.py
+++ Python/Programmers/codingTest/Level 0/n의 배수 고르기.py
@@ -5,3 +5,13 @@
 # 3     [4, 5, 6, 7, 8, 9, 10, 11, 12]  [6, 9, 12]
 # 5     [1, 9, 3, 10, 13, 5]            [10, 5]
 # 12    [2, 100, 120, 600, 12, 12]      [120, 600, 12, 12]
+
+
+# 내가 푼 코드 
+def solution(n, numlist):
+    answer = []
+    for i in numlist:
+        if i % n == 0:
+            answer.append(i) 
+            
+    return answer

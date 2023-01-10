45763cfddca2263c5c4b0be8a1b51e40a7d905d4
kmonguu <50946547+kmonguu@users.noreply.github.com>
2023-01-10 21:52:18 +0900

    Create 잘라서 배열로 저장하기.js

---
+++ JavaScript/Programmers/level 0/잘라서 배열로 저장하기.js
@@ -0,0 +1,8 @@
+// 문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 반환
+
+// 예제
+// my_str                n     result
+// "abc1Addfggg4556b"	6     ["abc1Ad", "dfggg4", "556b"]
+// "abcdef123"	        3     ["abc", "def", "123"]
+
+// 내가 푼 코드

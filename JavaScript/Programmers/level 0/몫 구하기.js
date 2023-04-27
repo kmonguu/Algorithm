649b6d781bdbf7ce6c66eadb65aa76f111cd208e
kmonguu <50946547+kmonguu@users.noreply.github.com>
2023-04-27 21:13:13 +0900

    Create 몫 구하기.js

---
+++ JavaScript/Programmers/level 0/몫 구하기.js
@@ -0,0 +1,9 @@
+// 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성
+
+// 풀이 1
+function solution(num1, num2) {
+    return Math.floor(num1/num2)
+}
+
+// 풀이 2
+const solution = (num1, num2) => Math.floor(num1/num2) 

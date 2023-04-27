b3120aace698ae333390392b935dfa75d365acf4
kmonguu <50946547+kmonguu@users.noreply.github.com>
2023-04-27 20:48:24 +0900

    Create 두 수의 차.js

---
+++ JavaScript/Programmers/level 0/두 수의 차.js
@@ -0,0 +1,9 @@
+// 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성
+
+// 풀이 1
+function solution(num1, num2) {
+    return num1 - num2
+}
+
+// 풀이 2
+const solution = (num1, num2) => num1- num2 

84aaecc1b7f7fd41cec23efb616c9483ae8c5f13
kmonguu <50946547+kmonguu@users.noreply.github.com>
2023-04-27 21:09:36 +0900

    Create 두 수의 곱.js

---
+++ JavaScript/Programmers/level 0/두 수의 곱.js
@@ -0,0 +1,9 @@
+// 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성
+
+// 풀이 1
+function solution(num1, num2) {
+    return num1*num2
+}
+
+// 풀이 2
+const solution = (num1, num2) => num1*num2

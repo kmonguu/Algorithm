d215b21e6593b8f57aefec02948b3bd2870ac392
kmonguu <skdus8009@gmail.com>
2025-01-08 15:12:09 +0900

    update[완전탐색]: 모의고사

--- JavaScript/Programmers/level 1/모의고사.js
+++ JavaScript/Programmers/level 1/모의고사.js
@@ -2,30 +2,16 @@ function solution(answers) {
   const first = [1, 2, 3, 4, 5];
   const second = [2, 1, 2, 3, 2, 4, 2, 5];
   const third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-  const students = [
-    repeatArrayToLength(first, answers.length),
-    repeatArrayToLength(second, answers.length),
-    repeatArrayToLength(third, answers.length),
-  ];
 
   let result = [0, 0, 0];
-  for (let i in students) {
-    answers.forEach((answer, index) => {
-      if (students[i][index] === answer) {
-        result[i] += 1;
-      }
-    });
+  for (let i in answers) {
+    if (answers[i] === first[i % first.length]) result[0] += 1;
+    if (answers[i] === second[i % second.length]) result[1] += 1;
+    if (answers[i] === third[i % third.length]) result[2] += 1;
   }
   const max = Math.max(...result);
-  const maxIndices = result
-    .map((num, idx) => (num === max ? idx + 1 : -1))
-    .filter((idx) => idx !== -1);
-  return maxIndices;
-}
-function repeatArrayToLength(arr, length) {
-  const repeated = [];
-  while (repeated.length < length) {
-    repeated.push(...arr);
-  }
-  return repeated.slice(0, length);
+  const maxIndex = result
+    .map((num, index) => (num === max ? index + 1 : -1))
+    .filter((num) => num !== -1);
+  return maxIndex;
 }
